b6b7c8b1a85b6f13af52e73f9764abd32090715f
Jekaterina <jevilt@ttu.ee>
2020-03-29 00:06:31 +0200

    Routing and views

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import { PLATFORM } from "aurelia-framework";
 
 export class App {
-  
+  constructor() {
+
+  };
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Admin App';
+    config.map([
+      {route: '', name: 'index', moduleId: PLATFORM.moduleName('index'), title: 'My orders'},
+      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('favorites'), title: 'My favorites'},
+      {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('user'), title: 'My settings'}
+    ]);
+  }
 }
