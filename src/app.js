9ca0799dbaf872b425a473c39dd0aca73c1bae54
Jekaterina <jevilt@ttu.ee>
2020-04-12 19:32:53 +0300

    Fomantic UI implementation and routing

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ export class App {
     this.router = router;
     config.title = 'Admin App';
     config.map([
-      {route: '', name: 'index', moduleId: PLATFORM.moduleName('index'), title: 'My orders'},
-      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('favorites'), title: 'My favorites'},
-      {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('user'), title: 'My settings'}
+      {route: '', name: 'index', moduleId: PLATFORM.moduleName('index'), title: 'Orders', settings: {data: 'home'}},
+      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('favorites'), title: 'Favorites', settings: {data: 'star outline'}},
+      {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('user'), title: 'Settings', settings: {data: 'user cog'}}
     ]);
   }
 }
