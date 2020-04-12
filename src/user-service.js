ea453fc9c6ddb163d2bf14f599c49c175c2689d9
Jekaterina <jevilt@ttu.ee>
2020-04-13 02:37:43 +0300

    User service: get method for new endpoints

---
+++ src/user-service.js
@@ -0,0 +1,31 @@
+import { HttpClient } from 'aurelia-fetch-client';
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
+export class UserService {
+    constructor() {
+        this.client = new HttpClient;
+    
+        const baseUrl = 'http://161.35.21.253:4567/api/';
+    
+        this.client.configure(config => {
+          config.withBaseUrl(baseUrl)
+                .withDefaults({
+                mode: 'cors',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+        })
+      }
+    
+      async getUser() {
+        try {
+          const response = await this.client.fetch('contact');
+          console.log(response);
+          return response.json();
+        } catch (e) {
+          console.log("User:", e);
+        }
+      }
+}
--- src/user.js
+++ src/user.js
@@ -1,3 +1,11 @@
+import { UserService } from './user-service';
+
 export class User {
-    
+    constructor() {
+        this.userService = new UserService();
+    }
+
+    async attached() {
+        this.users = await this.userService.getUser();
+    }
 }
