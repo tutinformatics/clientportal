fb8e6f4b347d4bd12b52ecb5459219fcbd9c535f
Karis Karindi <kkarin@ttu.ee>
2020-04-19 00:54:05 +0300

    Added invoices view. Modal is not working yet.

--- src/invoices.js
+++ src/invoices.js
@@ -1,3 +1,18 @@
 export class Invoices {
-    
 }
+
+//Modal ei tööta
+//kasutasin seda materjali https://jsfiddle.net/xcu93840/
+/*
+$(document).ready(function(){
+  $('.test-modal').modal();
+  $('.modal-btn').click(function(){
+    $('.test-modal').modal('show');
+  })
+  $('.test-modal').modal('setting', 'centered', false);
+  $('.test-modal').modal('setting', 'closable', false);
+});
+ */
+
+
+
