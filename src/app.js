88168c73afb67cdff075912dc264653bb6769785
Jekaterina <jevilt@ttu.ee>
2020-04-23 15:32:42 +0300

    Structure, router refactoring.

--- src/app.js
+++ src/app.js
@@ -10,13 +10,13 @@ export class App {
     this.router = router;
     config.title = 'Admin App';
     config.map([
-      {route: '', name: 'index', moduleId: PLATFORM.moduleName('index'), title: 'Orders', settings: {data: 'home'}},
-      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('favorites'), title: 'Favorites', settings: {data: 'star outline'}},
-      {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('user'), title: 'Settings', settings: {data: 'user cog'}},
-      {route: 'cart', name: 'cart', moduleId: PLATFORM.moduleName('cart'), title: 'Cart', settings: {data: 'shopping cart icon'}},
-      {route: 'offers', name: 'offers', moduleId: PLATFORM.moduleName('offers'), title: 'Offers', settings: {data: 'fire alternate icon'}},
-      {route: 'orders', name: 'orders', moduleId: PLATFORM.moduleName('orders'), title: 'Completed orders', settings: {data: 'box icon'}},
-      {route: 'invoices', name: 'invoices', moduleId: PLATFORM.moduleName('invoices'), title: 'Invoices', settings: {data: 'file alternate outline icon'}}
+      {route: '', name: 'index', moduleId: PLATFORM.moduleName('./views/order-views/index'), title: 'Orders', settings: {data: 'home'}},
+      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('./views/order-views/favorites'), title: 'Favorites', settings: {data: 'star outline'}},
+      {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('./views/user-views/user'), title: 'Settings', settings: {data: 'user cog'}},
+      {route: 'cart', name: 'cart', moduleId: PLATFORM.moduleName('./views/cart-view/cart'), title: 'Cart', settings: {data: 'shopping cart icon'}},
+      {route: 'offers', name: 'offers', moduleId: PLATFORM.moduleName('./views/order-views/offers'), title: 'Offers', settings: {data: 'fire alternate icon'}},
+      {route: 'orders', name: 'orders', moduleId: PLATFORM.moduleName('./views/order-views/orders'), title: 'Completed orders', settings: {data: 'box icon'}},
+      {route: 'invoices', name: 'invoices', moduleId: PLATFORM.moduleName('./views/invoice-views/invoices'), title: 'Invoices', settings: {data: 'file alternate outline icon'}}
     ]);
   }
 }
