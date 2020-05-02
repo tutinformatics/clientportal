1c8ca350c2e695ca284dd7fe44314490d335c3a8
Karis Karindi <kkarin@ttu.ee>
2020-05-02 14:54:37 +0300

    Both user settings locations are back.

--- src/app.js
+++ src/app.js
@@ -14,12 +14,8 @@ export class App {
       {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('./views/order-views/favorites'), title: 'Favorites', settings: {data: 'star outline'}},
       {route: 'cart', name: 'cart', moduleId: PLATFORM.moduleName('./views/cart-view/cart'), title: 'Cart', settings: {data: 'shopping cart icon'}},
       {route: 'offers', name: 'offers', moduleId: PLATFORM.moduleName('./views/order-views/offers'), title: 'Offers', settings: {data: 'fire alternate icon'}},
-      {route: 'invoices', name: 'invoices', moduleId: PLATFORM.moduleName('./views/invoice-views/invoices'), title: 'Invoices', settings: {data: 'file alternate outline icon'}}
-      //{route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('./views/user-views/user'), title: 'Settings', settings: {data: 'user cog'}}
+      {route: 'invoices', name: 'invoices', moduleId: PLATFORM.moduleName('./views/invoice-views/invoices'), title: 'Invoices', settings: {data: 'file alternate outline icon'}},
+      {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('./views/user-views/user'), title: 'Settings', settings: {data: 'user cog'}}
     ]);
-    config.mapRoute(
-      {route: 'user', moduleId: PLATFORM.moduleName('./views/user-views/user'), settings: {data: 'user cog'}}
-
-    )
   }
 }
