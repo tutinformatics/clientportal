0ccee227ea2a20e9cc88b916c16e9f7688744abe
Karis Karindi <kkarin@ttu.ee>
2020-05-02 15:07:20 +0300

    Added modal for invoice details. Needs fixing tho.

--- src/views/invoice-views/invoices.js
+++ src/views/invoice-views/invoices.js
@@ -1,13 +1,21 @@
 import {DialogController} from 'aurelia-dialog';
-  
+
 export class Invoices {
   static inject = [DialogController];
   person = { firstName: '' };
   constructor(controller){
+    $(document).ready(function() {
+      $('.modal-btn').click(function() {
+        $('.test-modal').modal('show');
+        $('.test-modal').modal('setting', 'centered', false);
+        $('.test-modal').modal('setting', 'closable', true);
+      })
+    });
     this.controller = controller;
   }
   activate(person){
     this.person = person;
   }
+
 }
 
