5eb7d55d5b250f8a0a30665478a13a8b859ba192
Jekaterina <jevilt@ttu.ee>
2020-05-04 02:40:37 +0300

    Order service test with noexpiration token

--- src/views/order-views/index.js
+++ src/views/order-views/index.js
@@ -1,3 +1,11 @@
-export class Index {
+import { OrderService } from '../../_services/order-service';
 
+export class Index {
+    constructor() {
+        this.orderService = new OrderService();
+    }
+
+    async attached() {
+        this.orders = await this.orderService.getOrders();
+    }
 }
--- src/views/user-views/user.js
+++ src/views/user-views/user.js
@@ -1,4 +1,4 @@
-import { UserService } from './user-service';
+import { UserService } from '../../_services/user-service';
 
 export class User {
     constructor() {
