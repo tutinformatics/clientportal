cadb49f8e79a26e544d6811786215eb192e2f2fd
Jekaterina <jevilt@ttu.ee>
2020-05-04 00:57:51 +0300

    Firendlier UI and rearranged menu.

--- src/app.js
+++ src/app.js
@@ -10,11 +10,12 @@ export class App {
     this.router = router;
     config.title = 'Admin App';
     config.map([
-      {route: '', name: 'index', moduleId: PLATFORM.moduleName('./views/order-views/index'), title: 'Orders', settings: {data: 'box icon'}},
-      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('./views/order-views/favorites'), title: 'Favorites', settings: {data: 'star outline'}},
+      {route: '', name: 'index', moduleId: PLATFORM.moduleName('./views/cart-view/cart'), title: 'OFBiz', settings: {data: 'java'}},
       {route: 'cart', name: 'cart', moduleId: PLATFORM.moduleName('./views/cart-view/cart'), title: 'Cart', settings: {data: 'shopping cart icon'}},
-      {route: 'offers', name: 'offers', moduleId: PLATFORM.moduleName('./views/order-views/offers'), title: 'Offers', settings: {data: 'fire alternate icon'}},
+      {route: 'quotes', name: 'quotes', moduleId: PLATFORM.moduleName('./views/order-views/quotes'), title: 'Quotes', settings: {data: 'fire alternate icon'}},
+      {route: 'orders', name: 'orders', moduleId: PLATFORM.moduleName('./views/order-views/index'), title: 'Orders', settings: {data: 'box icon'}},
       {route: 'invoices', name: 'invoices', moduleId: PLATFORM.moduleName('./views/invoice-views/invoices'), title: 'Invoices', settings: {data: 'file alternate outline icon'}},
+      {route: 'favorites', name: 'favorites', moduleId: PLATFORM.moduleName('./views/order-views/favorites'), title: 'Favorites', settings: {data: 'star outline'}},
       {route: 'user', name: 'user-settings', moduleId: PLATFORM.moduleName('./views/user-views/user'), title: 'Settings', settings: {data: 'user cog'}}
     ]);
   }
