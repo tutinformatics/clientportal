700762cd84792e91436b7131969f9778c702cf27
tajoot <tajoot@ttu.ee>
2020-05-04 08:27:05 +0300

    Refactor services

--- src/_services/invoice-service.js
+++ src/_services/invoice-service.js
@@ -1,46 +1,15 @@
-import { HttpClient } from "aurelia-fetch-client";
 import { autoinject } from 'aurelia-framework';
-
-
-const bearerTokenPrefix = 'Bearer '
-const noExpirationToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyTG9naW5JZCI6ImFkbWluIiwiaXNzIjoiQXBhY2hlT0ZCaXoiLCJleHAiOjE1ODc4NDI5OTk5OTk3MTgsImlhdCI6MTU4Nzg0MDkxOH0.3hZCbPuEoqQOUTYws1UtPToVuCZrQfaAVYkZIkPvAVd3m1cN-scUpIYErZFGTmMMfYHTEoMlbNlTG5l2GfkDVg'
+import {Service} from "./service";
 
 @autoinject
-export class InvoiceService {
-    baseUrl = "api/generic/v1/";
-
-    constructor() {
-        this.client = new HttpClient();
-        this.client.configure(config => {
-            config
-              .withBaseUrl(this.baseUrl)
-              .withDefaults({
-                headers: {
-                  'Accept': 'application/json',
-                  'X-Requested-With': 'Fetch',
-                  'Authorization': bearerTokenPrefix + noExpirationToken
-                }
-              })
-              .withInterceptor({
-                request(request) {
-                  console.info(`Requesting ${request.method} ${request.url}`);
-                  return request;
-                },
-                response(response) {
-                  console.info(`Received ${response.status} ${response.url}`);
-                  return response;
-                }
-              });
-          });
-        }
-
+export class InvoiceService extends Service {
     getInvoices() {
         return this.client
-        .fetch("entities/Invoice")
-        .then(response => response.json())
-        .catch(reason => {
-            console.error(reason);
-            return [];
+            .fetch("entities/Invoice")
+            .then(response => response.json())
+            .catch(reason => {
+                console.error(reason);
+                return [];
         });
     }
 }
---
+++ src/_services/service.js
@@ -0,0 +1,35 @@
+import { HttpClient } from "aurelia-fetch-client";
+import { autoinject } from 'aurelia-framework';
+
+const bearerTokenPrefix = 'Bearer '
+const noExpirationToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyTG9naW5JZCI6ImFkbWluIiwiaXNzIjoiQXBhY2hlT0ZCaXoiLCJleHAiOjE1ODc4NDI5OTk5OTk3MTgsImlhdCI6MTU4Nzg0MDkxOH0.3hZCbPuEoqQOUTYws1UtPToVuCZrQfaAVYkZIkPvAVd3m1cN-scUpIYErZFGTmMMfYHTEoMlbNlTG5l2GfkDVg'
+
+@autoinject
+export class Service {
+    baseUrl = "api/generic/v1/";
+
+    constructor() {
+        this.client = new HttpClient();
+        this.client.configure(config => {
+            config
+                .withBaseUrl(this.baseUrl)
+                .withDefaults({
+                    headers: {
+                        'Accept': 'application/json',
+                        'X-Requested-With': 'Fetch',
+                        'Authorization': bearerTokenPrefix + noExpirationToken
+                    }
+                })
+                .withInterceptor({
+                    request(request) {
+                        console.info(`Requesting ${request.method} ${request.url}`);
+                        return request;
+                    },
+                    response(response) {
+                        console.info(`Received ${response.status} ${response.url}`);
+                        return response;
+                    }
+                });
+        });
+    }
+}
